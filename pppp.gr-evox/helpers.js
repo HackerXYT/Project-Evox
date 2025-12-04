634cb50b4ddfa809af9af362010c7665f2bc7520
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2025-12-05 00:35:48 +0200

    some

---
+++ pppp.gr-evox/helpers.js
@@ -0,0 +1,74 @@
+function continueAnyway() {
+    document.getElementById("evxWarning").style.display = 'none'
+    sessionStorage.setItem("continueAnyway", 'true')
+}
+
+function learnMore() {
+    const element = document.getElementById('scrollIndicator');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function readMore(article) {
+    const blog_title = document.getElementById("blog-title")
+    const blog_content = document.getElementById("blog-content")
+    const articles = {
+        1: {
+            page: "content-blog-1.html",
+            title: "Ηλεκτρονευρογράφημα - Ηλεκτρομυογράφημα"
+        },
+        2: {
+            page: "content-blog-2.html",
+            title: "Υπερηχογραφία Νεύρων"
+        },
+        3: {
+            page: "content-blog-3.html",
+            title: "Εφαργμογές της αλλαντικής τοξίνης (Botox-Dysport) στη Νευρολογία"
+        },
+        4: {
+            page: "content-blog-4.html",
+            title: "Βελονισμός - Ηλεκτροβελονισμός - Ωτοβελονισμός"
+        }
+    }
+    const popup_container = document.getElementById("popup_container")
+    if (articles[article]) {
+        const picked = articles[article]
+        blog_title.innerHTML = picked.title
+        fetch(picked.page)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(innerHtml => {
+                blog_content.innerHTML = innerHtml
+                popup_container.querySelectorAll(".popup").forEach(element => {
+                    element.style.transform = "translateY(100vh)"
+                })
+                popup_container.style.display = 'flex'
+                setTimeout(function () {
+                    popup_container.style.backdropFilter = "brightness(60%)"
+                    popup_container.querySelectorAll(".popup").forEach(element => {
+                        element.style.transform = "translateY(0vh)"
+                    })
+                }, 100)
+            })
+            .catch(error => {
+                alert(`Error!: ${error}`)
+            });
+
+
+    }
+}
+
+function closePopup() {
+    const popup_container = document.getElementById("popup_container")
+    popup_container.querySelectorAll(".popup").forEach(element => {
+        element.style.transform = "translateY(100vh)"
+    })
+    popup_container.style.backdropFilter = ""
+    setTimeout(function () {
+        popup_container.style.display = 'none'
+
+    }, 300)
+}
