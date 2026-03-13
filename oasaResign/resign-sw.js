a4c14c905858c473a86032f1f8be287a30f426d2
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2026-03-14 00:08:21 +0200

    Update resign-sw.js

--- oasaResign/resign-sw.js
+++ oasaResign/resign-sw.js
@@ -13,8 +13,8 @@ self.addEventListener('notificationclick', function (event) {
   );
 });
 
-const STATIC_CACHE_NAME = 'static-cache-v119';
-const APP_CACHE_NAME = 'app-cache-v119';
+const STATIC_CACHE_NAME = 'static-cache-v120';
+const APP_CACHE_NAME = 'app-cache-v120';
 const CACHE_STATIC = [
   '/oasaResign/',
   '/oasaResign/index.html',
